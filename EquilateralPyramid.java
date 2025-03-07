f510d03e7f8c4583ba8410eb6deab5a9f709b4ba
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-07 18:22:25 +0530

    Implement area, perimeter and volume calculation for EquilateralPyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -10,3 +10,22 @@ class EquilateralPyramid extends Shape implements Volume {
         this.base = base;
         this.height = height;
     }
+
+    // Implement area calculation
+    @Override
+    double calculateArea() {
+        return base * base + 2 * base * Math.sqrt((base * base / 4) + (height * height));
+    }
+
+    // Implement perimeter calculation
+    @Override
+    double calculatePerimeter() {
+        return 4 * base;
+    }
+
+    // Implement volume calculation
+    @Override
+    public double calculateVolume() {
+        return (1.0 / 3.0) * base * base * height;
+    }
+}
