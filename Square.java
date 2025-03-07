d2be10cff58b86a6be72ccf7b12948fe3570a23f
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-07 18:15:29 +0530

    class for square and constructor of rectangle with equal sides for Square

---
+++ Square.java
@@ -0,0 +1,8 @@
+//Square.java
+
+// Class for Square, inherits from Rectangle
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);  // Calls Rectangle constructor with equal sides
+    }
+}
