49f197605a331c43303b34591f486cc84ef788d1
Vraj Patel <vrajbpatel1719@gmail.com>
2025-03-07 18:01:20 +0530

    Abstract class and Constructor to intialize shape

---
+++ Shape.java
@@ -0,0 +1,10 @@
+//Shape.java
+
+// Abstract class representing a general shape
+abstract class Shape {
+    protected String shapeName;
+
+    // Constructor to initialize the shape name
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
